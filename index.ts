45d739ac74d23783db0c53cf191dd6c4424a0ee8
bramirez96 <bran.ramirez.don@gmail.com>
2024-05-02 14:30:52 -0700

    Project updates v0.9.2

--- index.ts
+++
@@ -1,9 +0,0 @@
-// ! Copyright (c) 2024, Brandon Ramirez, brr.dev
-
-export * from "./src/actions";
-export * from "./src/classes";
-export * from "./src/features";
-export * from "./src/gameControls";
-export * from "./src/gameTypes";
-export * from "./src/items";
-export * from "./src/utils";
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// ! Copyright (c) 2024, Brandon Ramirez, brr.dev
+
+export * from "./actions";
+export * from "./classes";
+export * from "./features";
+export * from "./gameControls";
+export * from "./gameTypes";
+export * from "./items";
+export * from "./utils";
